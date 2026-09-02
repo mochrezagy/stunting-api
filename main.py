22781594ac5187c65caaa071c4c63d7f752ffd7e
Kelompok 9 <muhamadreza429@gmail.com>
2026-09-02 14:44:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 with open(os.path.join(BASE_DIR, 'scaler.pkl'), 'rb') as f:
     scaler = pickle.load(f)
 
 with open(os.path.join(BASE_DIR, 'model_stunting.pkl'), 'rb') as f:
     model = pickle.load(f)
-
+    
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"status": "API Stunting Ready!"})
